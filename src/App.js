2395aa9be96d6ae11de3ac659fae333e22724f4e
tonyhigver <tonyhigver@gmail.com>
2025-09-08 10:00:00 +0200

    Frontend: actualizar LandingPage, agregar YouTubeConnectedPage y App.js con rutas OAuth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import YouTubeConnectedPage from "./components/YouTubeConnectedPage";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/youtube-connected" element={<YouTubeConnectedPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/YouTubeConnectedPage.jsx
@@ -0,0 +1,9 @@
+export default function YouTubeConnectedPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-600">
+      <h1 className="text-4xl font-bold text-white">
+        ¡Conexión con YouTube completada!
+      </h1>
+    </div>
+  );
+}
