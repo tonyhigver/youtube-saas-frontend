9ea49d6938b0c4b113820182684a17d59e7b9567
tonyhigver <tonyhigver@gmail.com>
2025-09-12 20:21:23 +0200

    Descripci

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
-import YouTubeConnectedPage from "./components/YouTubeConnectedPage";
+import UploadDisclaimerPage from "./components/UploadDisclaimerPage"; // nueva página
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/youtube-connected" element={<YouTubeConnectedPage />} />
+        <Route path="/upload-disclaimer" element={<UploadDisclaimerPage />} />
+        {/* Puedes agregar más rutas como /upload en el futuro */}
       </Routes>
     </Router>
   );
